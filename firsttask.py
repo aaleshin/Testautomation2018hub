8a984dbcfab2002b65f1fba0e4d81f55ca921bd5
Aleshin Aleksey <aaleshin@thumbtack.net>
2018-10-21 17:36:51 +0600

    add first task

--- firsttask.py
+++ firsttask.py
@@ -1,1 +1,14 @@
-test
+import requests
+
+class Metod:
+    def __init__(self):
+        name = input("Как тебя зовут?")
+        print(name.upper(), ", добро пожаловать в мир Python, раздолбай!")
+
+
+print("Привет, тестер!")
+a = Metod()
+
+r = requests.get('https://planner.thumbtack.net/employees/')
+print(r.status_code)
+print(r.headers['content-type'])
